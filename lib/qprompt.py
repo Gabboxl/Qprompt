d4e40b97b801581c5930e5fa64912c8582111a8d
Gabriele <gabboxl0@gmail.com>
2020-01-23 12:51:57 +0100

    fix formatting

--- lib/qprompt.py
+++ lib/qprompt.py
@@ -9,6 +9,7 @@ from __future__ import print_function
 
 import copy
 import ctypes
+import inspect
 import random
 import string
 import os
@@ -26,10 +27,13 @@ from functools import wraps
 # Handle Python 2/3 differences.
 if sys.version_info >= (3, 0):
     from io import StringIO
+
     tounicode = lambda s: s
 else:
     from StringIO import StringIO
+
     tounicode = lambda s: unicode(s, "utf-8")
+
 
 def _format_kwargs(func):
     """Decorator to handle formatting kwargs to the proper names expected by
@@ -44,6 +48,7 @@ def _format_kwargs(func):
     formats['msg'] = ["message"]
     formats['shw'] = ["show"]
     formats['vld'] = ["valid"]
+
     @wraps(func)
     def inner(*args, **kwargs):
         for k in formats.keys():
@@ -52,7 +57,9 @@ def _format_kwargs(func):
                     kwargs[k] = kwargs[v]
                     kwargs.pop(v)
         return func(*args, **kwargs)
+
     return inner
+
 
 ##==============================================================#
 ## SECTION: Global Definitions                                  #
@@ -91,6 +98,7 @@ SILENT = False
 #: If true, echo() returns the string to print.
 ECHORETURN = True
 
+
 ##==============================================================#
 ## SECTION: Class Definitions                                   #
 ##==============================================================#
@@ -98,35 +106,50 @@ ECHORETURN = True
 class StdinSetup:
     """Sets up stdin to be supplied via `setinput()`; a default context manager
     is provided by `stdin_setup`."""
+
     def __init__(self, stream=None):
         self._stream = stream or StringIO()
         self.original = sys.stdin
+
     def setup(self):
         sys.stdin = self._stream
+
     def teardown(self):
         sys.stdin = self.original
+
     def __enter__(self):
         self.setup()
         return self
+
     def __exit__(self, type, value, traceback):
         self.teardown()
+
+
 stdin_setup = StdinSetup()
+
 
 class StdinAuto:
     """Automatically set stdin using supplied list; a default context manager
     is provided by `stdin_auto`."""
+
     def __init__(self, auto=None):
         self.auto = auto or sys.argv[1:]
+
     def __enter__(self, auto=None):
         if self.auto:
             stdin_setup.setup()
             setinput("\n".join(self.auto))
+
     def __exit__(self, type, value, traceback):
         stdin_setup.teardown()
+
+
 stdin_auto = StdinAuto()
+
 
 class Menu:
     """Menu object that will show the associated MenuEntry items."""
+
     def __init__(self, *entries, **kwargs):
         """Initializes menu object. Any `kwargs` supplied will be passed as
         defaults to `show_menu()`."""
@@ -138,25 +161,30 @@ class Menu:
                 entry = (name, desc, entry)
             self.add(*entry)
         self._show_kwargs = kwargs
+
     def add(self, name, desc, func=None, args=None, krgs=None):
         """Add a menu entry."""
         self.entries.append(MenuEntry(name, desc, func, args or [], krgs or {}))
+
     def enum(self, desc, func=None, args=None, krgs=None):
         """Add a menu entry whose name will be an auto indexed number."""
-        name = str(len(self.entries)+1)
+        name = str(len(self.entries) + 1)
         self.entries.append(MenuEntry(name, desc, func, args or [], krgs or {}))
+
     def show(self, **kwargs):
         """Shows the menu. Any `kwargs` supplied will be passed to
         `show_menu()`."""
         show_kwargs = copy.deepcopy(self._show_kwargs)
         show_kwargs.update(kwargs)
         return show_menu(self.entries, **show_kwargs)
+
     def run(self, name):
         """Runs the function associated with the given entry `name`."""
         for entry in self.entries:
             if entry.name == name:
                 run_func(entry)
                 break
+
     def main(self, auto=None, loop=False, quit=("q", "Quit"), **kwargs):
         """Runs the standard menu main logic. Any `kwargs` supplied will be
         pass to `Menu.show()`. If `argv` is provided to the script, it will be
@@ -168,6 +196,7 @@ class Menu:
           - loop (bool) - If true, the menu will loop until quit.
           - quit ((str,str)) - If provided, adds a quit option to the menu.
         """
+
         def _main():
             global _AUTO
             if quit:
@@ -191,6 +220,7 @@ class Menu:
                 note = "Menu does not loop, single entry."
                 result = self.show(note=note, **kwargs)
             return result
+
         global _AUTO
         if _AUTO:
             return _main()
@@ -198,26 +228,31 @@ class Menu:
             with stdin_auto:
                 return _main()
 
+
 class Wrap(object):
     """Context manager that wraps content between horizontal lines."""
+
     @_format_kwargs
     def __init__(self, width=None, char="", **kwargs):
         hdr = kwargs.get('hdr', "")
         char = char or HRCHAR
         width = width or HRWIDTH
-        top = "/" + getline(char, width-1)
+        top = "/" + getline(char, width - 1)
         if hdr:
             top = stridxrep(top, 3, " ")
-            for i,c in enumerate(hdr):
-                top = stridxrep(top, i+4, hdr[i])
-            top = stridxrep(top, i+5, " ")
+            for i, c in enumerate(hdr):
+                top = stridxrep(top, i + 4, hdr[i])
+            top = stridxrep(top, i + 5, " ")
         self.top = top
-        self.bot = "\\" + getline(char, width-1)
+        self.bot = "\\" + getline(char, width - 1)
+
     def __enter__(self):
         echo(self.top)
         return self
+
     def __exit__(self, type, value, traceback):
         echo(self.bot)
+
 
 ##==============================================================#
 ## SECTION: Function Definitions                                #
@@ -231,13 +266,15 @@ stridxrep = lambda s, i, r: "".join([(s[x] if x != i else r) for x in range(len(
 
 #: Allows stdin to be set via function; use with `stdin_setup` context.
 setinput = lambda x: [
-        sys.stdin.seek(0),
-        sys.stdin.truncate(0),
-        sys.stdin.write(x),
-        sys.stdin.seek(0)]
+    sys.stdin.seek(0),
+    sys.stdin.truncate(0),
+    sys.stdin.write(x),
+    sys.stdin.seek(0)]
 
 try:
     print("", end="", flush=True)
+
+
     def echo(text="", end="\n", flush=True):
         if not SILENT:
             print(text, end=end, flush=flush)
@@ -254,6 +291,7 @@ except TypeError:
         if ECHORETURN:
             return text + end
 
+
 @_format_kwargs
 def show_limit(entries, **kwargs):
     """Shows a menu but limits the number of entries shown at a time.
@@ -261,8 +299,8 @@ def show_limit(entries, **kwargs):
     limit = kwargs.pop('limit', 5)
     if limit <= 0:
         return show_menu(entries, **kwargs)
-    istart = 0 # Index of group start.
-    iend = limit # Index of group end.
+    istart = 0  # Index of group start.
+    iend = limit  # Index of group end.
     dft = kwargs.pop('dft', None)
     if type(dft) == int:
         dft = str(dft)
@@ -273,12 +311,12 @@ def show_limit(entries, **kwargs):
         if istart < 0:
             istart = 0
             iend = limit
-        unext = len(entries) - iend # Number of next entries.
-        uprev = istart # Number of previous entries.
-        nnext = "" # Name of 'next' menu entry.
-        nprev = "" # Name of 'prev' menu entry.
-        dnext = "" # Description of 'next' menu entry.
-        dprev = "" # Description of 'prev' menu entry.
+        unext = len(entries) - iend  # Number of next entries.
+        uprev = istart  # Number of previous entries.
+        nnext = ""  # Name of 'next' menu entry.
+        nprev = ""  # Name of 'prev' menu entry.
+        dnext = ""  # Description of 'next' menu entry.
+        dprev = ""  # Description of 'prev' menu entry.
         group = copy.deepcopy(entries[istart:iend])
         names = [i.name for i in group]
         if unext > 0:
@@ -314,6 +352,7 @@ def show_limit(entries, **kwargs):
         else:
             return result
 
+
 @_format_kwargs
 def show_menu(entries, **kwargs):
     """Shows a menu with the given list of `MenuEntry` items.
@@ -343,6 +382,7 @@ def show_menu(entries, **kwargs):
     msg = []
     if limit:
         return show_limit(entries, **kwargs)
+
     def show_banner():
         banner = "-- MENU"
         if hdr:
@@ -353,6 +393,7 @@ def show_menu(entries, **kwargs):
             return
         for i in entries:
             msg.append("  (%s) %s" % (i.name, i.desc))
+
     valid = [i.name for i in entries]
     if type(dft) == int:
         dft = str(dft)
@@ -387,6 +428,7 @@ def show_menu(entries, **kwargs):
     except:
         return getattr(entry, "name")
 
+
 def run_func(entry):
     """Runs the function associated with the given MenuEntry."""
     if entry.func:
@@ -397,6 +439,7 @@ def run_func(entry):
         if entry.krgs:
             return entry.func(**entry.krgs)
         return entry.func()
+
 
 def enum_menu(strs, menu=None, *args, **kwargs):
     """Enumerates the given list of strings into returned menu.
@@ -411,6 +454,7 @@ def enum_menu(strs, menu=None, *args, **kwargs):
         menu.enum(s)
     return menu
 
+
 def cast(val, typ=int):
     """Attempts to cast the given value to the given type otherwise None is
     returned."""
@@ -420,8 +464,10 @@ def cast(val, typ=int):
         val = None
     return val
 
+
 @_format_kwargs
-def ask(msg="Enter input", fmt=None, dft=None, vld=None, shw=True, blk=False, hlp=None, qstr=True, multi=False, **kwargs):
+def ask(msg="Enter input", fmt=None, dft=None, vld=None, shw=True, blk=False, hlp=None, qstr=True, multi=False,
+        **kwargs):
     """Prompts the user for input and returns the given answer. Optionally
     checks if answer is valid.
 
@@ -435,6 +481,7 @@ def ask(msg="Enter input", fmt=None, dft=None, vld=None, shw=True, blk=False, hl
         supplying a default value will disable accepting blank input.
     """
     global _AUTO
+
     def print_help():
         lst = [v for v in vld if not callable(v)]
         if blk:
@@ -456,6 +503,7 @@ def ask(msg="Enter input", fmt=None, dft=None, vld=None, shw=True, blk=False, hl
             echo("[HELP] Extra notes: " + hlp)
         if blk:
             echo("[HELP] Input may be blank.")
+
     vld = vld or []
     hlp = hlp or ""
     if not hasattr(vld, "__iter__"):
@@ -463,7 +511,7 @@ def ask(msg="Enter input", fmt=None, dft=None, vld=None, shw=True, blk=False, hl
     if not hasattr(fmt, "__call__"):
         fmt = lambda x: x  # NOTE: Defaults to function that does nothing.
     msg = "%s%s" % (QSTR if qstr else "", msg)
-    dft = fmt(dft) if dft != None else None # Prevents showing [None] default.
+    dft = fmt(dft) if dft != None else None  # Prevents showing [None] default.
     if dft != None:
         msg += " [%s]" % (dft if type(dft) is str else repr(dft))
         vld.append(dft)
@@ -474,8 +522,10 @@ def ask(msg="Enter input", fmt=None, dft=None, vld=None, shw=True, blk=False, hl
         if blk and "" not in vld:
             vld.append("")
         # NOTE: The following fixes a Py3 related bug found in `0.8.1`.
-        try: vld = sorted(vld)
-        except: pass
+        try:
+            vld = sorted(vld)
+        except:
+            pass
     msg += ISTR
     ans = None
     while ans is None:
@@ -515,6 +565,7 @@ def ask(msg="Enter input", fmt=None, dft=None, vld=None, shw=True, blk=False, hl
                 ans = None
     return ans
 
+
 @_format_kwargs
 def ask_yesno(msg="Proceed?", dft=None, **kwargs):
     """Prompts the user for a yes or no answer. Returns True for yes, False
@@ -531,11 +582,13 @@ def ask_int(msg="Enter an integer", dft=None, vld=None, hlp=None, **kwargs):
     vld = vld or [int]
     return ask(msg, dft=dft, vld=vld, fmt=partial(cast, typ=int), hlp=hlp)
 
+
 @_format_kwargs
 def ask_float(msg="Enter a float", dft=None, vld=None, hlp=None, **kwargs):
     """Prompts the user for a float."""
     vld = vld or [float]
     return ask(msg, dft=dft, vld=vld, fmt=partial(cast, typ=float), hlp=hlp)
+
 
 @_format_kwargs
 def ask_str(msg="Enter a string", dft=None, vld=None, shw=True, blk=True, hlp=None, **kwargs):
@@ -543,17 +596,21 @@ def ask_str(msg="Enter a string", dft=None, vld=None, shw=True, blk=True, hlp=No
     vld = vld or [str]
     return ask(msg, dft=dft, vld=vld, shw=shw, blk=blk, hlp=hlp)
 
+
 #: Alias for `ask_str(shw=False)`.
 ask_pass = partial(ask_str, msg="Enter password", shw=False)
+
 
 def ask_captcha(length=4):
     """Prompts the user for a random string."""
     captcha = "".join(random.choice(string.ascii_lowercase) for _ in range(length))
     ask_str('Enter the following letters, "%s"' % (captcha), vld=[captcha, captcha.upper()], blk=False)
 
+
 def pause():
     """Pauses and waits for user interaction."""
     getpass("Press ENTER to continue...")
+
 
 def clear():
     """Clears the console."""
@@ -561,6 +618,7 @@ def clear():
         call("cls", shell=True)
     else:
         call("clear", shell=True)
+
 
 def status(*args, **kwargs):
     """Prints a status message at the start and finish of an associated
@@ -594,6 +652,7 @@ def status(*args, **kwargs):
         qprompt.status("Doing a thing...", myfunc, [arg1], {krgk: krgv})
         # [!] Doing a thing... DONE.
     """
+
     def decor(func):
         @wraps(func)
         def wrapper(*args, **krgs):
@@ -601,19 +660,26 @@ def status(*args, **kwargs):
             result = func(*args, **krgs)
             echo(fin, flush=True)
             return result
+
         return wrapper
+
     fin = kwargs.pop('fin', "DONE.")
     args = list(args)
     if len(args) > 1 and callable(args[1]):
         msg = args.pop(0)
         func = args.pop(0)
-        try: fargs = args.pop(0)
-        except: fargs = []
-        try: fkrgs = args.pop(0)
-        except: fkrgs = {}
+        try:
+            fargs = args.pop(0)
+        except:
+            fargs = []
+        try:
+            fkrgs = args.pop(0)
+        except:
+            fkrgs = {}
         return decor(func)(*fargs, **fkrgs)
     msg = args.pop(0)
     return decor
+
 
 def fatal(msg, exitcode=1, **kwargs):
     """Prints a message then exits the program. Optionally pause before exit
@@ -625,21 +691,26 @@ def fatal(msg, exitcode=1, **kwargs):
         pause()
     sys.exit(exitcode)
 
+
 def error(msg, **kwargs):
     """Prints error message to console. Returns printed string."""
     return echo("[ERROR] " + msg, **kwargs)
+
 
 def warn(msg, **kwargs):
     """Prints warning message to console. Returns printed string."""
     return echo("[WARNING] " + msg, **kwargs)
 
+
 def info(msg, **kwargs):
     """Prints info message to console. Returns printed string."""
     return echo("[INFO] " + msg, **kwargs)
 
+
 def alert(msg, **kwargs):
     """Prints alert message to console. Returns printed string."""
     return echo("[!] " + msg, **kwargs)
+
 
 def hrule(width=None, char=None):
     """Outputs or returns a horizontal line of the given character and width.
@@ -648,10 +719,12 @@ def hrule(width=None, char=None):
     char = char or HRCHAR
     return echo(getline(char, width))
 
+
 def title(msg):
     """Sets the title of the console window."""
     if sys.platform.startswith("win"):
         ctypes.windll.kernel32.SetConsoleTitleW(tounicode(msg))
+
 
 @_format_kwargs
 def wrap(item, args=None, krgs=None, **kwargs):
@@ -670,6 +743,7 @@ def wrap(item, args=None, krgs=None, **kwargs):
             item(*args, **krgs)
         else:
             echo(item)
+
 
 def _guess_name(desc, taken=None):
     """Attempts to guess the menu entry name from the function name."""
@@ -690,9 +764,11 @@ def _guess_name(desc, taken=None):
         count += 1
     return name
 
+
 def _guess_desc(fname):
     """Attempts to guess the menu entry description from the function name."""
     return fname.title().replace("_", " ")
+
 
 ##==============================================================#
 ## SECTION: Main Body                                           #
